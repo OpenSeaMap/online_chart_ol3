27f176a1ae8bbbfff7835b200c2a320a86ff580d
Axel Utech <axel.utech@gmail.com>
2016-04-26 17:29:08 +0200

    change deployment

--- gulpfile.js/tasks/deploy.js
+++ gulpfile.js/tasks/deploy.js
@@ -5,24 +5,18 @@ var open    = require('open')
 var os      = require('os')
 var package = require('../../package.json')
 var path    = require('path')
-var sftp = require('gulp-sftp');
 
 var settings = {
-  url: package.homepage,
   src: path.join(config.root.dest, '/**/*'),
-  sftp: {
-    host: '222142.webhosting69.1blu.de',
-    user: 'ftp222142-2697028',
-    remotePath: '/hp/cn/ac/fs/www/osm-dev-map'
+  ghPages: {
+    cacheDir: path.join(os.tmpdir(), package.name),
+    force: true
   }
 }
 
 var deployTask = function() {
   return gulp.src(settings.src)
-    .pipe(sftp(settings.sftp))
-    .on('end', function(){
-      open(settings.url)
-    })
+    .pipe(ghPages(settings.ghPages))
 }
 
 gulp.task('deploy', ['production'], deployTask)
