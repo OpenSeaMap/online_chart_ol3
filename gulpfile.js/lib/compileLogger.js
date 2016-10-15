866ff8ed3076d5085057961ee513fe0912f0bbee
Axel Utech <axel.utech@gmail.com>
2016-10-15 19:32:25 +0200

    fix compiling

--- gulpfile.js/lib/compileLogger.js
+++ gulpfile.js/lib/compileLogger.js
@@ -4,14 +4,17 @@ var handleErrors = require('./handleErrors')
 var fs = require('fs');
 var config       = require('../config')
 var path   = require('path')
-
+var mkdirp = require('mkdirp');
 
 module.exports = function(err, stats) {
   if(err) throw new gutil.PluginError("webpack", err)
 
   var statColor = stats.compilation.warnings.length < 1 ? 'green' : 'yellow'
 
-  fs.writeFileSync(path.join(config.root.debug, 'webpack.json'), JSON.stringify(stats.toJson()));
+  mkdirp(config.root.debug, function(err) {
+    if(err) throw new gutil.PluginError("webpack-debug-output", err)
+    fs.writeFileSync(path.join(config.root.debug, 'webpack.json'), JSON.stringify(stats.toJson()));
+  });
 
   if(stats.compilation.errors.length > 0) {
     stats.compilation.errors.forEach(function(error){
