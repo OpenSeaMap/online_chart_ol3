fdf47cdf6826ac0a54048631bca3bf2aafb7948b
Axel Utech <axel.utech@gmail.com>
2017-01-19 18:18:14 +0100

    fix display of download bundles
    Firefox did not allow to click the link as it was nested inside a <button>

--- src/javascripts/features/downloadBundles/downloadDisplay.js
+++ src/javascripts/features/downloadBundles/downloadDisplay.js
@@ -6,7 +6,7 @@
 
 import React, { PropTypes } from 'react'
 
-import { ListGroupItem, Collapse } from 'react-bootstrap'
+import { Panel } from 'react-bootstrap'
 import { FormattedMessage, FormattedRelative } from 'react-intl'
 import { DownloadLink } from '../../components/misc/Links'
 
@@ -15,46 +15,40 @@ export const DownloadDisplay = ({
   onResultClicked, onResultHovered, onResultUnhover,
   feature
 }) => (
-  <ListGroupItem
-    active={isClicked}
-    bsStyle={isHovered && !isClicked ? 'info' : null}
+  <Panel
+    bsStyle={isClicked ? 'primary' : isHovered ? 'warning' : 'info'}
     onClick={onResultClicked}
+    collapsible expanded={isClicked}
     onMouseOut={onResultUnhover}
-    onMouseOver={onResultHovered} >
-    <h4 className='list-group-item-heading'>
-      {feature.name}
-    </h4>
-    <Collapse in={isClicked}>
-      <div className='list-group-item-text'>
-        <FormattedMessage
-          id='download-file-date'
-          defaultMessage='Date: {rawDate, date, short} ({formatedRelative})'
-          description='Display a file date as a absolute date and a relative timespan'
-          values={{
-            rawDate: new Date(feature.date),
-            formatedRelative: <FormattedRelative value={feature.date} />
-          }} />
-        <br />
-        <FormattedMessage
-          id='download-file-format'
-          defaultMessage='Format: {format}'
-          description='Display a file format name'
-          values={{format: feature.format}} />
-        <br />
-        <FormattedMessage
-          id='download-file-application'
-          defaultMessage='Application: {app}'
-          description='Display an application name'
-          values={{app: feature.app}} />
-        <br />
-        <DownloadLink href={feature.downloadUrl}>
-          <FormattedMessage
-            id='download-file-download'
-            defaultMessage='Download file' />
-        </DownloadLink>
-      </div>
-    </Collapse>
-  </ListGroupItem>
+    onMouseOver={onResultHovered}
+    header={feature.name}>
+    <FormattedMessage
+      id='download-file-date'
+      defaultMessage='Date: {rawDate, date, short} ({formatedRelative})'
+      description='Display a file date as a absolute date and a relative timespan'
+      values={{
+        rawDate: new Date(feature.date),
+        formatedRelative: <FormattedRelative value={feature.date} />
+      }} />
+    <br />
+    <FormattedMessage
+      id='download-file-format'
+      defaultMessage='Format: {format}'
+      description='Display a file format name'
+      values={{format: feature.format}} />
+    <br />
+    <FormattedMessage
+      id='download-file-application'
+      defaultMessage='Application: {app}'
+      description='Display an application name'
+      values={{app: feature.app}} />
+    <br />
+    <DownloadLink href={feature.downloadUrl}>
+      <FormattedMessage
+        id='download-file-download'
+        defaultMessage='Download file' />
+    </DownloadLink>
+  </Panel>
 
 )
 
