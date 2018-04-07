e50fcb04283da48a68fb9b0fd6437525c8a2a0c5
Axel Utech <axel.utech@gmail.com>
2018-04-07 11:32:04 +0200

    fix lint errors

--- src/javascripts/features/downloadBundles/downloadDisplay.js
+++ src/javascripts/features/downloadBundles/downloadDisplay.js
@@ -10,20 +10,20 @@ import { ListGroupItem, Collapse } from 'react-bootstrap'
 import { FormattedMessage, FormattedRelative } from 'react-intl'
 import { DownloadLink } from '../../components/misc/Links'
 
-function humanFileSize(bytes, si) {
-    var thresh = si ? 1000 : 1024;
-    if(Math.abs(bytes) < thresh) {
-        return bytes + ' B';
-    }
-    var units = si
-        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
-        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
-    var u = -1;
-    do {
-        bytes /= thresh;
-        ++u;
-    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
-    return bytes.toFixed(1)+' '+units[u];
+function humanFileSize (bytes, si) {
+  var thresh = si ? 1000 : 1024
+  if (Math.abs(bytes) < thresh) {
+    return bytes + ' B'
+  }
+  var units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+  var u = -1
+  do {
+    bytes /= thresh
+    ++u
+  } while (Math.abs(bytes) >= thresh && u < units.length - 1)
+  return bytes.toFixed(1) + ' ' + units[u]
 }
 
 export const DownloadDisplay = ({
