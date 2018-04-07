2e2fecf60fc99c6a83ac4bb081b999024be5aebc
Axel Utech <axel.utech@gmail.com>
2018-04-07 11:24:06 +0200

    chart bundles: show filesize

--- src/javascripts/features/downloadBundles/downloadDisplay.js
+++ src/javascripts/features/downloadBundles/downloadDisplay.js
@@ -10,6 +10,22 @@ import { ListGroupItem, Collapse } from 'react-bootstrap'
 import { FormattedMessage, FormattedRelative } from 'react-intl'
 import { DownloadLink } from '../../components/misc/Links'
 
+function humanFileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+}
+
 export const DownloadDisplay = ({
   isClicked, isHovered,
   onResultClicked, onResultHovered, onResultUnhover,
@@ -46,6 +62,12 @@ export const DownloadDisplay = ({
           defaultMessage='Application: {app}'
           description='Display an application name'
           values={{app: feature.app}} />
+        <br />
+        <FormattedMessage
+          id='download-file-filesize'
+          defaultMessage='Size: {size}'
+          description='Display the file size'
+          values={{size: humanFileSize(feature.filesize, false)}} />
         <br />
         <DownloadLink href={feature.url}>
           <FormattedMessage
